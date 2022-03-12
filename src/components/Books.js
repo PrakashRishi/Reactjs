795e921151d74ab9689e781c10bbd97d286f65a8
Rishi <rprakash@highwirepress.com>
2022-03-12 22:34:37 +0530

    video 5 completed

---
+++ src/components/Books.js
@@ -0,0 +1,7 @@
+function Books() {
+    return(
+        <h1>welcome to the Book store</h1>
+    )
+}
+
+export default Books;
---
+++ src/components/Students.js
@@ -0,0 +1,6 @@
+function Students() {
+    return(
+        <h2>Welcome to the Students section</h2>
+    );
+}
+export default Students;
